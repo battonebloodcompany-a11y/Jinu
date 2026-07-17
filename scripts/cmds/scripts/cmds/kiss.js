e631523c3f36516316f285d0b7f215e667e4d3e6
battonebloodcompany-a11y <battonebloodcompany@gmail.com>
2026-07-17 22:58:57 +0600

    Create kiss.js

---
+++ scripts/cmds/scripts/cmds/kiss.js
@@ -0,0 +1,56 @@
+const DIG = require("discord-image-generation");
+const fs = require("fs-extra");
+const path = require("path");
+
+module.exports = {
+	config: {
+		name: "kiss",
+		version: "1.0",
+		author: "Fahim + ChatGPT",
+		countDown: 5,
+		role: 0,
+		shortDescription: {
+			en: "Kiss someone"
+		},
+		longDescription: {
+			en: "Generate a kiss image"
+		},
+		category: "image",
+		guide: {
+			en: "{pn} @mention"
+		}
+	},
+
+	onStart: async function ({ event, message, usersData }) {
+		try {
+			const uid1 = event.senderID;
+			const uid2 = Object.keys(event.mentions)[0];
+
+			if (!uid2)
+				return message.reply("❌ | Tag someone.");
+
+			const avatar1 = await usersData.getAvatarUrl(uid1);
+			const avatar2 = await usersData.getAvatarUrl(uid2);
+
+			const img = await new DIG.Kiss().getImage(avatar1, avatar2);
+
+			const tmpDir = path.join(__dirname, "tmp");
+			await fs.ensureDir(tmpDir);
+
+			const filePath = path.join(tmpDir, `${uid1}_${uid2}_kiss.png`);
+
+			fs.writeFileSync(filePath, img);
+
+			await message.reply({
+				body: "😘 Kiss!",
+				attachment: fs.createReadStream(filePath)
+			});
+
+			fs.unlinkSync(filePath);
+
+		} catch (e) {
+			console.log(e);
+			message.reply("❌ Error:\n" + e.message);
+		}
+	}
+};
